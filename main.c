02d36cea635882e69362582994b3eebf5e8bfc61
Emil TOULOUSE <emil.toulouse@epita.fr>
2022-04-03 17:23:06 +0200

    edited main

---
+++ main.c
@@ -0,0 +1,5 @@
+#include "main.h"
+
+int main(int argc,char* argv[]){
+	int status = app(argc,argv);
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <gtk/gtk.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <err.h>
+#include "glade/interface.h"
+
+int main(int argc, char *argv[]);
+
+#endif
